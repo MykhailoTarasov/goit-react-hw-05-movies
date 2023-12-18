d1cd1df8aba059896751c081d84f2023c8c005fd
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-12-18 16:23:45 +0200

    first - to do components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import  App  from './components/App';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyle';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-05-movies">
+    <ThemeProvider theme={{}}>
       <App />
+      <GlobalStyle/>
+    </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
+
+
