1c123fb3162578e09bc07869e24c53cffa933557
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-12-18 15:09:15 +0200

    some stert

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+
+export const App = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/movies">Movies</NavLink>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<div>Home page</div>} />
+        <Route path="/movies" element={<div>Movies</div>} />
+
+        <Route path="/movies/:movieId" element={<div>Movie</div>} />
+        <Route path="/movies/:movieId/cast" element={<div>Cast</div>} />
+        <Route path="/movies/:movieId/reviews" element={<div>Reviews</div>} />
+      </Routes>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
