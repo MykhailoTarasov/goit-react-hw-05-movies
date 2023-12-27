9df907a56e5890d65c54ccae3161b2e542e019bb
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-12-27 17:12:19 +0200

    play

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/infoMoviesApp">
+    <BrowserRouter basename="/goit-react-hw-05-movies">
     <ThemeProvider theme={{}}>
       <App />
       <GlobalStyle/>
